b06be48330c92d5b674c6b78ed75761ea6f4a901
Alexrocc <alerocc70@gmail.com>
2023-11-08 17:08:03 +0100

    added small error prevention and added btn classes

--- script.js
+++ script.js
@@ -10,45 +10,48 @@ window.addEventListener("DOMContentLoaded", () => {
       const row = document.getElementsByClassName("row")[0];
       console.log(booksArr);
       booksArr.forEach((book) => {
-        const col = document.createElement("div");
-        col.classList.add("col-6", "col-md-4", "col-xl-3");
-        row.appendChild(col);
-        const cardDiv = document.createElement("div");
-        cardDiv.classList.add("card");
+        if (book.img) {
+          const col = document.createElement("div");
+          col.classList.add("col-6", "col-md-4", "col-xl-3");
+          row.appendChild(col);
+          const cardDiv = document.createElement("div");
+          cardDiv.classList.add("card");
 
-        const cardImg = document.createElement("img");
-        cardImg.classList.add("card-img-top");
-        cardImg.src = book.img;
-        cardDiv.appendChild(cardImg);
+          const cardImg = document.createElement("img");
+          cardImg.classList.add("card-img-top");
+          cardImg.src = book.img;
+          cardDiv.appendChild(cardImg);
 
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("card-body");
-        cardDiv.appendChild(cardBody);
+          const cardBody = document.createElement("div");
+          cardBody.classList.add("card-body");
+          cardDiv.appendChild(cardBody);
 
-        const bookTitle = document.createElement("h5");
-        bookTitle.classList.add("card-title");
-        bookTitle.innerText = book.title;
-        cardBody.appendChild(bookTitle);
+          const bookTitle = document.createElement("h5");
+          bookTitle.classList.add("card-title");
+          bookTitle.innerText = book.title;
+          cardBody.appendChild(bookTitle);
 
-        const bookCategory = document.createElement("p");
-        bookCategory.classList.add("card-text");
-        bookCategory.innerText = book.category;
-        cardBody.appendChild(bookCategory);
+          const bookCategory = document.createElement("p");
+          bookCategory.classList.add("card-text");
+          bookCategory.innerText = book.category;
+          cardBody.appendChild(bookCategory);
 
-        const bookPrice = document.createElement("p");
-        bookPrice.classList.add("card-text");
-        bookPrice.innerText = book.price;
-        cardBody.appendChild(bookPrice);
+          const bookPrice = document.createElement("p");
+          bookPrice.classList.add("card-text");
+          bookPrice.innerText = book.price;
+          cardBody.appendChild(bookPrice);
 
-        const removeBtn = document.createElement("button");
-        removeBtn.type = "button";
-        removeBtn.innerText = "Remove";
-        removeBtn.onclick = function (e) {
-          e.currentTarget.parentNode.parentNode.parentNode.remove();
-        };
-        cardBody.appendChild(removeBtn);
+          const removeBtn = document.createElement("button");
+          removeBtn.type = "button";
+          removeBtn.innerText = "Remove";
+          removeBtn.classList.add("btn", "btn-secondary");
+          removeBtn.onclick = function (e) {
+            e.currentTarget.parentNode.parentNode.parentNode.remove();
+          };
+          cardBody.appendChild(removeBtn);
 
-        col.appendChild(cardDiv);
+          col.appendChild(cardDiv);
+        }
       });
     })
     .catch((error) => console.log("ERROR: ", error));
